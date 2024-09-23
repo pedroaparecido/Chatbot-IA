e59002fc1be397424d8050b21f305a68aaacc471
Pedro Ap <pedroaparecidori@gmail.com>
2024-09-23 18:06:03 -0300

    Incluido Chat com IA server side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import Image from "next/image";
+'use client'
+
+import { Chat } from "@/components/Chat";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen bg-neutral-100 items-center justify-center">
-      <Card className="w-[440px] h-[600px] grid grid-rows-[min-content_1fr_min-content] shadow-2xl">
-        <CardHeader>
-          <CardTitle>Chat IA</CardTitle>
-          <CardDescription>Criação de chatbot com IA</CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <div className="flex gap-3 text-slate-600 text-small">
-            <Avatar>
-              <AvatarFallback>PA</AvatarFallback>
-              <AvatarImage className="rounded-full w-[50px]" src="https://github.com/pedroaparecido.png" />
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold text-slate-800">Humano:</span> Qual a sua opnião sobre javascript?
-            </p>
-          </div>
-          <div className="flex gap-3 text-slate-600 text-small">
-          <Avatar>
-              <AvatarFallback>PA</AvatarFallback>
-              <AvatarImage className="rounded-full w-[50px]" src="https://github.com/rocketseat.png" />
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold text-slate-800">IA:</span> Qual a sua opnião sobre javascript?
-            </p>
-          </div>
-        </CardContent>
-        <CardFooter className="space-x-2">
-          <Input placeholder="Como posso te ajudar?" />
-          <Button type="submit">Enviar</Button>
-        </CardFooter>
-      </Card>
+      <Chat />
     </div>
   );
 }
---
+++ src/components/Chat.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useChat } from "ai/react";
+
+export function Chat() {
+    const { messages, input, handleInputChange, handleSubmit } = useChat({
+        api: '/api/chat'
+    })
+
+    return(
+        <Card className="w-[440px] h-[600px] grid grid-rows-[min-content_1fr_min-content] shadow-2xl">
+        <CardHeader>
+          <CardTitle>Chat IA</CardTitle>
+          <CardDescription>Criação de chatbot com IA</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+            { messages.map(message => {
+                return (
+                    <div key={message.id} className="flex gap-3 text-slate-600 text-small">
+                        {message.role === 'user' &&
+                            <Avatar>
+                            <AvatarFallback>PA</AvatarFallback>
+                            <AvatarImage className="rounded-full w-[50px]" src="https://github.com/pedroaparecido.png" />
+                            </Avatar>
+                        }
+                        {message.role === 'assistant' &&
+                            <Avatar>
+                            <AvatarFallback>RS</AvatarFallback>
+                            <AvatarImage className="rounded-full w-[50px]" src="https://github.com/rocketseat.png" />
+                            </Avatar>
+                        }
+                        <p className="leading-relaxed">
+                        <span className="block font-bold text-slate-800">{message.role === 'user' ? 'Usuário' : 'AI'}:</span>
+                        {message.content}
+                        </p>
+                    </div>
+                )
+            })}
+        </CardContent>
+        <CardFooter>
+        <form className="space-x-2 w-full flex gap-2" onSubmit={handleSubmit}>
+            <Input placeholder="Como posso te ajudar?" value={input} onChange={handleInputChange} />
+            <Button type="submit">Enviar</Button>
+        </form>
+        </CardFooter>
+      </Card>
+    )
+}
